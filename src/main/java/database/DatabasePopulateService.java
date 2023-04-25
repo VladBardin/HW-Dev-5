94d75eafc63a1c3a7a1914eb79854b6fbd3d84ea
VladBardin <vladyslav.bardin@gmail.com>
2023-04-25 21:07:34 +0300

    Second commit

--- src/main/java/database/DatabasePopulateService.java
+++ src/main/java/database/DatabasePopulateService.java
@@ -3,18 +3,35 @@ package database;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 
 public class DatabasePopulateService {
     public static final String POPULATE_DB_SQL_FILE_PATH = "./sql/populate_db.sql";
+
     public static void main(String[] args) {
-        Database date = Database.getInstance();
+        Database database = Database.getInstance();
+        Connection connection = null;
+        PreparedStatement preparedStatement = null;
         try {
+            connection = database.getConnection();
             String sqlInit = String.join("\n", Files.readAllLines(Paths.get(POPULATE_DB_SQL_FILE_PATH)));
-            date.executeUpdate(sqlInit);
-        } catch (IOException e) {
+            preparedStatement = connection.prepareStatement(sqlInit);
+            preparedStatement.executeUpdate();
+        } catch (IOException | SQLException e) {
             throw new RuntimeException(e);
         } finally {
-            date.close();
+            try {
+                if (preparedStatement != null) {
+                    preparedStatement.close();
+                }
+                if (connection != null) {
+                    connection.close();
+                }
+            } catch (SQLException e) {
+                throw new RuntimeException(e);
+            }
         }
     }
 }
